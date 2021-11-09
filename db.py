8f274b3ed02f2cc43324ee57d125d089b145d3b1
Aitor Magán <amagan@conwet.com>
2021-11-09 20:46:51 +0100

    Fix min date

--- db.py
+++ db.py
@@ -45,7 +45,10 @@ class Influx:
         self.client.write_points(influx_data)
 
     def get_measurement_last_day(self, measurement: Measurement):
-        query = f"SELECT last(value), time from {measurement.value};"
-        result = self.client.query(query)
+        try:
+            query = f"SELECT last(value), time from a{measurement.value};"
+            result = self.client.query(query)
 
-        return datetime.strptime(list(result.items()[0][1])[0]["time"].split("T")[0], "%Y-%m-%d")
+            return datetime.strptime(list(result.items()[0][1])[0]["time"].split("T")[0], "%Y-%m-%d")
+        except IndexError:
+            return datetime(2021, 6, 1)
