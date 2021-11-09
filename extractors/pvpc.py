1be9b85ffbc4c1ea8d96e5cf47aba6fbe5349d2b
Aitor Magán <amagan@conwet.com>
2021-11-09 20:58:19 +0100

    Refactor code

---
+++ extractors/pvpc.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timedelta, date
+import os
+import requests
+from db import Measurement
+
+
+REE_TOKEN = os.environ.get("REE_TOKEN")
+
+
+def update_pvpc(influx_client):
+    day = influx_client.get_measurement_last_day(Measurement.PVPC) + timedelta(1)
+    tomorrow = datetime.combine(date.today() + timedelta(1), datetime.min.time())
+
+    while day <= tomorrow:
+        update_pvpc_day(influx_client, day)
+        day = day + timedelta(1)
+
+
+def update_pvpc_day(influx_client, day):
+    pvpc_price = get_day_pvpc(day)
+    influx_client.insert_stats(Measurement.PVPC, day, pvpc_price)
+
+    day_str = day.strftime("%d/%m/%Y")
+    print(f"PVPC for {day_str} processed...")
+
+
+def get_day_pvpc(day):
+    day_str = day.strftime("%Y-%m-%d")
+    url = f"https://api.esios.ree.es/indicators/1001?start_date={day_str}T00:00:00.000&end_date={day_str}T23:59:59.000&geo_ids%5B%5D=8741"
+    data = requests.get(url, headers={"Authorization": f"Token token=\"{REE_TOKEN}\""}).json()
+    return {datetime.fromisoformat(x["datetime"]).hour: x["value"] / 1000 for x in data["indicator"]["values"]}
---
+++ extractors/ufd.py
@@ -0,0 +1,60 @@
+from datetime import datetime, timedelta, date
+import os
+import requests
+from db import Measurement
+
+
+DNI = os.environ.get("UFD_USER")
+PASS = os.environ.get("UFD_PASS")
+
+
+UFD_HEADERS = {"X-Appversion": "1.0.0.0", "X-Application": "ACUFD", "X-AppClientSecret": "4CUFDW3b",
+               "X-Appclientid": "ACUFDWeb", "X-AppClient": "ACUFDW"}
+
+
+def update_consumption(influx_client):
+    ufd_token = login(DNI, PASS)
+    cupses = get_cups(DNI, ufd_token)
+    day = influx_client.get_measurement_last_day(Measurement.CONSUMPTION) + timedelta(1)
+    today = datetime.combine(date.today(), datetime.min.time())
+
+    while day < today:
+        update_consumption_day(influx_client, ufd_token, cupses, day)
+        day = day + timedelta(1)
+
+
+def update_consumption_day(influx_client, ufd_token, cupses, day):
+    for cups in cupses:
+        consumption_map = get_day_consumption(ufd_token, cups, day)
+        influx_client.insert_stats(Measurement.CONSUMPTION, day, consumption_map, {"cups": cups})
+
+    day_str = day.strftime("%d/%m/%Y")
+    print(f"Consumption for {day_str} processed...")
+
+
+def login(user, password):
+    res = requests.post("https://api.ufd.es/ufd/v1.0/login", json={"user": user, "password": password},
+                        headers=UFD_HEADERS)
+    return res.json()["accessToken"]
+
+
+def get_cups(user, token):
+    headers = get_udf_headers(token)
+    res = requests.get(f"https://api.ufd.es/ufd/v1.0/supplypoints?filter=documentNumber::{user}", headers=headers)
+    return [x["cups"] for x in res.json()["supplyPoints"]["items"]]
+
+
+def get_day_consumption(token, cups, day):
+    headers = get_udf_headers(token)
+    day_str = day.strftime("%d/%m/%Y")
+    res = requests.get(f"https://api.ufd.es/ufd/v1.0/consumptions?filter=nif::{DNI}%7Ccups::{cups}%7CstartDate::{day_str}%7CendDate::{day_str}%7Cgranularity::H%7Cunit::K%7Cgenerator::0%7CisDelegate::N%7CisSelfConsumption::0%7CmeasurementSystem::O",
+                       headers=headers)
+    consumptions = res.json()["items"][0]["consumptions"]["items"]
+
+    return {x["hour"] - 1: float(x["consumptionValue"].replace(",", ".")) for x in consumptions}
+
+
+def get_udf_headers(token):
+    headers = {"Authorization": f"Bearer {token}"}
+    headers.update(UFD_HEADERS)
+    return headers
